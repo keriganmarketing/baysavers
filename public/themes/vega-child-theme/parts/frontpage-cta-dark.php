302f890787ca0366091b8034610dc4dd822ffec6
Robert Crites III <robertcritesiii@Roberts-MacBook-Pro.local>
2019-02-08 10:30:00 -0600

    changes to contact form

--- public/themes/vega-child-theme/parts/frontpage-cta-dark.php
+++ public/themes/vega-child-theme/parts/frontpage-cta-dark.php
@@ -3,7 +3,7 @@
     <div class="container">
         <?php if(is_front_page()){ ?><h2 class="block-title">Join Us</h2><?php } ?>
         <div class="row">
-            <div class="recent-entry col">
+            <div class="recent-entry col-md-6 col-md-offset-3">
                 <h3>Email</h3>
                 <?php echo do_shortcode('[contact-form-7 id="24" title="Contact"]'); ?>
             </div>
