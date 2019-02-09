e2923bc90f945f5c0242427b5f365586f037d220
Robert Crites III <robertcritesiii@Roberts-MacBook-Pro.local>
2019-02-08 23:55:14 -0600

    footer fix

--- public/themes/vega-child-theme/page.php
+++ public/themes/vega-child-theme/page.php
@@ -12,7 +12,7 @@
 <?php get_template_part('parts/banner'); ?>
 
 <!-- ========== Page Content ========== -->
-<div class="section page-content bg-white">
+<div class="section page-content bg-white wrapper">
     <div class="container">
         <div class="row">
             
@@ -57,6 +57,7 @@
             
         </div>
     </div>
+    <div class="push"></div>
 </div>
 <!-- ========== /Page Content ========== -->
 
