601a8b176afa852e58c6042ad27a3b0df639f574
Robert Crites III <robertcritesiii@Roberts-MacBook-Pro.local>
2019-02-08 23:48:48 -0600

    footer fix

--- public/themes/vega-child-theme/footer.php
+++ public/themes/vega-child-theme/footer.php
@@ -12,25 +12,6 @@
 <div class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
-                                
-                <?php if ( has_nav_menu( 'footer' ) ) :  ?>
-                <!-- Navigation -->
-                <?php
-                wp_nav_menu( array(
-                    'theme_location'    => 'footer',
-                    'depth'             => 1,
-                    'container'         => '',
-                    'menu_class'        => 'nav-foot'
-                    )
-                );
-                ?>
-                <!-- /Navigation -->
-                <?php else: ?>
-                <?php vega_wp_example_nav_footer(); ?>
-                <?php endif; ?>
-                
-            </div>
             <div class="col-md-4">
                 <!-- Copyright and Credits -->
                 <?php $vega_wp_footer_copyright_message = vega_wp_get_option('vega_wp_footer_copyright_message'); ?>
