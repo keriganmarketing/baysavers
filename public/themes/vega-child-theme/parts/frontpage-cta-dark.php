97bcdbec2e3aca6fc7405faf0bf54bace5034d32
Robert Crites III <robertcritesiii@Roberts-MacBook-Pro.local>
2019-02-13 13:11:21 -0500

    Heading edit and toggle selections

--- public/themes/vega-child-theme/parts/frontpage-cta-dark.php
+++ public/themes/vega-child-theme/parts/frontpage-cta-dark.php
@@ -37,15 +37,10 @@ if($vega_wp_frontpage_cta_dark_bg_image != ''){
 
 ?>
 <!-- ========== Call to Action ========== -->
-<div class="section">
-    <div class="container">
-        <?php if(is_front_page()){ ?><h2 class="block-title">Join Us</h2><?php } ?>
-        <div class="row">
-            <div class="recent-entry col-md-6 col-md-offset-3">
-                <h3>Email</h3>
-                <?php echo do_shortcode('[contact-form-7 id="24" title="Contact"]'); ?>
-            </div>
-        </div>
+<div class="shadow"></div>
+<div class="section frontpage-cta frontpage-cta1 <?php echo $class ?>" <?php echo $parallax_str; ?> id="<?php echo esc_attr($vega_wp_frontpage_cta_dark_section_id); ?>">
+    <div class="container wow zoomIn description">
+        <div class="description"><?php echo $vega_wp_frontpage_cta_dark_content; ?></div>
     </div>
 </div>
 <!-- ========== /Call to Action ========== -->
