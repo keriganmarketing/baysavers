bcd4f6728ab49f1b7e27372a533a70f0e9d21847
Robert Crites III <robertcritesiii@Roberts-MacBook-Pro.local>
2019-02-13 13:04:32 -0500

    Heading edit and toggle selections

--- public/themes/vega-child-theme/parts/frontpage-cta-dark.php
+++ public/themes/vega-child-theme/parts/frontpage-cta-dark.php
@@ -1,4 +1,17 @@
-<?php ?>
+<?php
+
+$vega_wp_frontpage_cta_dark = vega_wp_get_option('vega_wp_frontpage_cta_dark'); 
+
+if($vega_wp_frontpage_cta_dark2 == 'Y') { 
+
+    global $post; //SiteOrign Page Builder fix
+    $cta_page = get_post(vega_wp_get_option('vega_wp_frontpage_cta_dark_content')); 
+    $post = $cta_page ;
+    $vega_wp_frontpage_cta_dark_content = apply_filters( 'the_content', $cta_page->post_content );
+
+}
+?>
+
 <div class="section">
     <div class="container">
         <?php if(is_front_page()){ ?><h2 class="block-title">Join Us</h2><?php } ?>
